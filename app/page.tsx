c687aadcdd7d2ab5adae45af3ae0f6cbe465cca6
Miki <mickeytuts2401@gmail.com>
2025-12-24 15:43:55 +0100

    testing updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div className="text-5xl text-center">HomePage</div>;
+  return (
+    <div className="text-5xl text-center">
+      <h2>Home Page</h2>
+      <p className="text-xl bg-red-300">Welcome to my portfolio website!</p>
+    </div>
+  );
 };
 
 export default HomePage;

cf6066fbd1a45fd9bcd6a5932787b8395854ac9a
Miki <mickeytuts2401@gmail.com>
2025-12-28 16:55:00 +0100

    add SectionHeading

--- component/Home/Home.tsx
+++ component/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero/Hero";
+import About from "./About/About";
 
 const Home = () => {
   return (
     <div className="text-5xl text-center overflow-hidden">
       <Hero />
+      <About />
     </div>
   );
 };

f3786f7c2253c2b7648c26b86c4f47572a27206d
Miki <mickeytuts2401@gmail.com>
2026-01-02 08:20:25 +0100

    add to SkillCards

--- component/Home/Home.tsx
+++ component/Home/Home.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Hero from "./Hero/Hero";
 import About from "./About/About";
+import Services from "./Services/Services";
+import Project from "./Project/Project";
+import Skills from "./Skills/Skills";
+import Review from "./Review/Review";
 
 const Home = () => {
   return (
     <div className="text-5xl text-center overflow-hidden">
       <Hero />
       <About />
+      <Services />
+      <Project />
+      <Skills />
+      <Review />
     </div>
   );
 };
